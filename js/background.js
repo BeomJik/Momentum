9842a0d54dbfbc055e1ad00fc5519147e9ac3d8a
BeomJik <qjarmsrkt0201@gmail.com>
2022-11-06 23:12:38 +0900

    이미지 생성 방식 수정

--- js/background.js
+++ js/background.js
@@ -1,9 +1,13 @@
-const images = ["bg1.jpeg", "bg2.jpeg", "bg3.jpeg"];
+const images = ["bg1.jpg", "bg2.jpg", "bg3.jpg", "bg4.jpg"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-const bgImage = document.createElement('img');
+// const bgImage = document.createElement('img');
 
-bgImage.src = `img/${chosenImage}`
+// bgImage.src = `img/${chosenImage}`
 
-document.body.appendChild(bgImage);
+// document.body.appendChild(bgImage);
+
+const body = document.body;
+
+body.style.backgroundImage = `url(/img/${chosenImage})`
