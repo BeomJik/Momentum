74e5c1cb918964e8b1958368e882ddf3f59f3cd3
BeomJik <qjarmsrkt0201@gmail.com>
2022-11-06 18:47:19 +0900

    날씨 기능 구현

--- js/weather.js
+++ js/weather.js
@@ -1,8 +1,17 @@
 function onGeoOk(position) {
-    console.log(position);
-    let 위도 = position.coords.latitude;
-    let 경도 = position.coords.longitude;
-    console.log(`너의 위치  위도 : ${위도} 경도 : ${경도}`);
+    let lat = position.coords.latitude;
+    let lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+    fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+        const weatherContainer = document.querySelector(`#weather`);
+        const weather = weatherContainer.querySelectorAll(`#weather span`)[0];
+        const city = weatherContainer.querySelectorAll(`#weather span`)[1];
+        weather.innerText = data.weather[0].main;
+        city.innerText = data.name;
+
+    });
 }
 function onGeoError() {
     alert(`Can't find you. No weather for you.`);    
