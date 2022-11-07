5542a0ca939297269fc7fe903d0a4d6a075bc29d
BeomJik <beomjikdp@gmail.com>
2022-11-07 18:43:12 +0900

    이미지 경로 수정

--- js/background.js
+++ js/background.js
@@ -10,4 +10,4 @@ const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 const body = document.body;
 
-body.style.backgroundImage = `url(/img/${chosenImage})`
+body.style.backgroundImage = `url(./img/${chosenImage})`
