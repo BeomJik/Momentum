2c915338c7cd8302ed0d09a5890c327e622cc255
BeomGeun <beomjikdp@gmail.com>
2022-11-07 11:12:13 +0900

    이름변경 기능 추가

--- js/greetings.js
+++ js/greetings.js
@@ -1,6 +1,7 @@
 const loginForm = document.querySelector('#login-form');
 const loginInput = document.querySelector('#login-form input');
 const greeting = document.querySelector('#greeting');
+const reName = document.querySelector('#rename');
 
 const HIDE_CLASSNAME = 'hide';
 const USERNAME_KEY = 'username';
@@ -9,15 +10,17 @@ const savedUsername = localStorage.getItem(USERNAME_KEY);
 if (savedUsername == null) {
     loginForm.classList.remove(HIDE_CLASSNAME);
     loginForm.addEventListener('submit',onLogomSubmit);
+    reName.classList.add(HIDE_CLASSNAME);
 } else {
     paintGreetings(savedUsername);
 }
+
+reName.addEventListener('click',onReName);
 
 function paintGreetings(username){
     greeting.classList.remove(HIDE_CLASSNAME);
     greeting.innerText = `Hello  ${username}`;
 }
-
 
 function onLogomSubmit(e) {
     e.preventDefault();
@@ -25,4 +28,15 @@ function onLogomSubmit(e) {
     const userName = loginInput.value;
     localStorage.setItem(USERNAME_KEY,userName);
     paintGreetings(userName);
+    reName.classList.remove(HIDE_CLASSNAME);
+   
+}
+
+function onReName(e) {
+    e.preventDefault();
+    localStorage.removeItem(USERNAME_KEY);
+    loginForm.classList.remove(HIDE_CLASSNAME);
+    loginForm.addEventListener('submit',onLogomSubmit);
+    greeting.innerText = ``;
+    reName.classList.add(HIDE_CLASSNAME);
 }
